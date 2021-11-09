e68464b79c675b78df276e5aa200fc36c564d946
Andrew saed <77155374+andrew2237@users.noreply.github.com>
2021-11-09 12:11:01 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import Typers from "./components/Typers";
 
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="84434503-4bc8-4b0e-8a1c-93334f662893"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatProps) => <ChatFeed {...chatProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+      renderIsTyping={(typers) => {
+        <Typers {...typers} />;
+      }}
+    />
   );
 }
 
---
+++ src/components/Typers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Typers = ({ chats }) => {
+  console.log(chats);
+  return (
+    <div>
+      <h4>{chats}</h4>
+    </div>
+  );
+};
+export default Typers;
